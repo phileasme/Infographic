ea5cce52821ff960d3400cf9467d6074038dd118
phileasme <phileas.hocquard@gmail.com>
2015-11-27 00:13:57 +0000

    rest of the prototype

---
+++ app/src/main/java/com/phileas/infographic/model/Countries.java
@@ -0,0 +1,14 @@
+package com.phileas.infographic.model;
+
+
+import java.util.ArrayList;
+
+public class Countries {
+    ArrayList<Country> countries;
+        public Countries(){
+            countries = new ArrayList<Country>();
+        }
+    public ArrayList<Country> getCountries (){
+        return countries;
+    }
+}
