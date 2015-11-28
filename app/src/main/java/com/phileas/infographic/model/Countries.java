e1268f8ad1e12b6b119ec83e81001793ef66a076
Phileas <phileas.hocquard@kcl.ac.uk>
2015-11-28 15:21:20 +0000

    rest of the prototype

--- app/src/main/java/com/phileas/infographic/model/Countries.java
+++ app/src/main/java/com/phileas/infographic/model/Countries.java
@@ -5,10 +5,53 @@ import java.util.ArrayList;
 
 public class Countries {
     ArrayList<Country> countries;
+    Boolean hasDoneFirstAdd;
         public Countries(){
             countries = new ArrayList<Country>();
+            hasDoneFirstAdd = false;
         }
     public ArrayList<Country> getCountries (){
         return countries;
     }
+
+    /** Method that verifies if a country with a specific name exist in the collect of Countries
+     * @param name
+     * @return
+     */
+    public boolean countrywithNameExist(String name){
+        for(Country c : countries){
+            if( c.getName().equals(name)){
+                return  true;
+            }
+        }
+        return false;
+    }
+
+    /** Method to add countries while reducing iteration
+     * we consider that a first time we add countries
+     * we do not require to iterate through a complete list.
+     * @param country
+     */
+    public void AddCountry(Country country){
+        if(hasDoneFirstAdd == false){
+        countries.add(country);}
+        else{
+            updateCountries(country);
+        }
+    }
+
+    /** Updates a country in our Arraylist of countries
+     * @param country
+     */
+    public void updateCountries(Country country) {
+        for(Country c : countries){
+            if( c.getId().equals(country.getId())){
+                c = country;
+            }
+            else if(c.equals(countries.get(countries.size()-1))){
+                countries.add(country);
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/phileas/infographic/model/Country.java
+++ app/src/main/java/com/phileas/infographic/model/Country.java
@@ -2,32 +2,49 @@ package com.phileas.infographic.model;
 
 import android.util.Pair;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 /**
  * Created by Peace on 26/11/2015.
  */
 public class Country {
+    /** Name of the country and it's id **/
     private String name;
+    private String id;
+
+
     /**
-     *
+     * Map of unique indicators
+     *                   (Year/Indicator) ,Value
      */
-                      //(Year/Indicator) ,Value
     private HashMap<Pair<Integer,String>,Double> indicators;
 
-    public Country(String name){
+    /**
+     * Country constructor
+     * @param name
+     * @param id
+     */
+    public Country(String name,String id){
         this.name = name;
+        this.id = id;
     }
 
+    /** Adds an Indicator or replaces the value of a previous one
+     *
+     * @param indicator
+     * @param year
+     * @param value
+     */
     public void addIndicator(String indicator,int year, double value){
         Pair<Integer,String> primaryTuple = new Pair<>(year,indicator);
        indicators.put(primaryTuple,value);
     }
 
-    /** Removes all the elements of a certain date **/
+    /** Removes all the elements of a certain date
+     *
+     * @param year
+     */
     public void removeYear(int year){
         for(Map.Entry<Pair<Integer,String>,Double> entry : indicators.entrySet()){
             Pair<Integer,String> pair = entry.getKey();
@@ -37,8 +54,28 @@ public class Country {
         }
     }
 
+    /**
+     *  Retrieve the list of indicators
+     * @return  indicators
+     */
     public  HashMap<Pair<Integer,String>,Double> getIndicators(){
         return indicators;
     }
 
+
+    /**
+     * Retrieve the name of the country
+     * @return
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * Retrieve the Id of the country
+     * @return
+     */
+    public String getId(){
+        return id;
+    }
 }
