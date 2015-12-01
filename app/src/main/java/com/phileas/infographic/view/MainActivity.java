848c39255c81f38051df294ed55dee036076d4a0
phileasme <phileas.hocquard@gmail.com>
2015-12-01 00:06:34 +0000

    Finished Reading Files Local Feature.

--- app/src/main/java/com/phileas/infographic/view/MainActivity.java
+++ app/src/main/java/com/phileas/infographic/view/MainActivity.java
@@ -10,8 +10,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.phileas.infographic.R;
+import com.phileas.infographic.controller.ReadAllAssets;
 import com.phileas.infographic.controller.ReadFromJson;
 import com.phileas.infographic.controller.ValueAdapter;
+import com.phileas.infographic.model.Countries;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,17 +22,24 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    Countries countries;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-       ReadFromJson readj = new ReadFromJson("IC.BUS.EASE.XQ.json",this.getApplicationContext());
 
-            readj.loadJSONFromAsset();
-
+        //Returns a defined Countries class filled with indicator information
+        ReadAllAssets retrieveAllLocalCountriesInfo = new ReadAllAssets();
+        countries = retrieveAllLocalCountriesInfo.ReadAllAssetFiles("",this.getBaseContext());
+        /**
+         *
+         * Pass this to the main UI controller "countries.getCountries()" and that will give you a collection of countries.
+         * Like this : UIMainController uiMainController = new UIMainController(countries.getCountries());
+         * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         *                  Once you have made the UIMainController of course.
+         */
     }
 
 
