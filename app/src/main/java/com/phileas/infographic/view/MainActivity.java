34529f834c5d75e715350e50231571ec264b286a
Shokryah Mohammadi <shokryah.mohammadi@kcl.ac.uk>
2015-12-02 14:07:41 +0000

    merged with countriesList branch

--- app/src/main/java/com/phileas/infographic/view/MainActivity.java
+++ app/src/main/java/com/phileas/infographic/view/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -30,6 +31,9 @@ import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.renderer.BarChartRenderer;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.phileas.infographic.R;
+import com.phileas.infographic.controller.CountryAdapter;
+import com.phileas.infographic.controller.ReadAllAssets;
+import com.phileas.infographic.model.Countries;
 
 
 import java.util.ArrayList;
@@ -37,28 +41,60 @@ import java.util.FormatFlagsConversionMismatchException;
 import java.util.List;
 
 public class MainActivity extends Activity {
-
-
     private PieChart pieChart;
     private EditText editText;
     private ListView listView;
-
-
+    Countries countries;
     private float [] yData = {50,23,10,50, 60};
     private String [] xData = {"Uk", "Singapor", "America", "China", "Austria"};
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+            @Override
+            public void onCreate(final Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
 
-        super.onCreate(savedInstanceState);
+                //Returns a defined Countries class filled with indicator information
+                ReadAllAssets retrieveAllLocalCountriesInfo = new ReadAllAssets();
+                countries = retrieveAllLocalCountriesInfo.ReadAllAssetFiles("",this.getBaseContext());
+                /**
+                 *
+                 * Pass this to the main UI controller "countries.getCountries()" and that will give you a collection of countries.
+                 * Like this : UIMainController uiMainController = new UIMainController(countries.getCountries());
+                 * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                 *                  Once you have made the UIMainController of course.
+                 */
 
-        setContentView(R.layout.activity_main);
-        editText = (EditText) findViewById(R.id.searchBox);
-        listView = (ListView) findViewById(R.id.listCountries);
+
+                ArrayList<String> countryName = new ArrayList<String>();
 
 
+                for (int i=0; i<countries.getCountries().size(); i++){
+                    String name = countries.getCountries().get(i).getName();
+                    countryName.add(name);
+                }
 
+                final CountryAdapter countryAdapter = new CountryAdapter(countryName, this.getBaseContext());
+                listView = (ListView) findViewById(R.id.list_view);
+                listView.setAdapter(countryAdapter);
+                CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox);
+                editText = (EditText) findViewById(R.id.txt_search);
+                editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        countryAdapter.getFilter().filter(s);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
 
 
         pieChart=(PieChart) findViewById(R.id.pieChart);
@@ -104,6 +140,7 @@ public class MainActivity extends Activity {
 
 
     }
+
     private ArrayList<BarDataSet> getBarDataSet() {
         ArrayList<BarDataSet> dataSets = null;
 
@@ -159,7 +196,6 @@ public class MainActivity extends Activity {
         xAxis.add("2010");
         return xAxis;
     }
-
 
 
 
