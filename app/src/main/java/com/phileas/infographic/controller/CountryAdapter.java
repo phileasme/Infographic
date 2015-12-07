cf1a0a571d9947ff675e6ce377adfcc8a8414bfa
mukanova_e <elizabeta.mukanova@kcl.ac.uk>
2015-12-07 14:57:28 +0000

    Added buttons

--- app/src/main/java/com/phileas/infographic/controller/CountryAdapter.java
+++ app/src/main/java/com/phileas/infographic/controller/CountryAdapter.java
@@ -1,52 +1,47 @@
 package com.phileas.infographic.controller;
-
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.TextView;
-
 import com.phileas.infographic.R;
 import com.phileas.infographic.model.Country;
-
 import java.util.ArrayList;
-public class CountryAdapter extends BaseAdapter implements Filterable {
 
-    private ArrayList<String> countriesList;
 
-    private ArrayList<String> filteredCountries;
+public class CountryAdapter extends ArrayAdapter<Country> implements Filterable {
 
-    private LayoutInflater layoutInflater;
+
+
+    private ArrayList<Country> filteredCountries= new ArrayList<>();
+    private ArrayList<String> countriesList= new ArrayList<>();
+
 
     private ValueFilter valueFilter;
+    private ViewHolder viewHolder;
+    private boolean[] itemChecked;
+    int checked=0;
 
-    public CountryAdapter(ArrayList<String> countriesList, Context context) {
 
-        this.countriesList=countriesList;
+    public CountryAdapter(Context context, int resource, ArrayList<Country> a) {
+        super(context, resource,a);
 
-        this.filteredCountries=countriesList;
+        for(int i=0; i<a.size(); i++)
+        {
+            this.countriesList.add(a.get(i).getName());
+        }
 
-        layoutInflater=LayoutInflater.from(context);
-
-        getFilter();
+                this.itemChecked = new boolean[a.size()];
+                getFilter();
     }
 
-    //How many items are in the data set represented by this Adapter.
-    @Override
-    public int getCount() {
 
-        return countriesList.size();
-    }
-
-    //Get the data item associated with the specified position in the data set.
-    @Override
-    public Object getItem(int position) {
-
-        return countriesList.get(position);
-    }
 
     //Get the row id associated with the specified position in the list.
     @Override
@@ -57,34 +52,43 @@ public class CountryAdapter extends BaseAdapter implements Filterable {
 
     //Get a View that displays the data at the specified position in the data set.
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        Country country = getItem(position);
 
-        ViewHolder viewHolder;
+        viewHolder = null;
+        Log.v("ConvertView", String.valueOf(position));
 
         if(convertView==null) {
+            convertView =LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
 
             viewHolder=new ViewHolder();
 
-            convertView=layoutInflater.inflate(R.layout.itemlist,null);
 
-            viewHolder.textView=(TextView)convertView.findViewById(R.id.txt_listitem);
+            viewHolder.textView=(TextView)convertView.findViewById(R.id.txt_listitem1);
+            viewHolder.checkBox = (CheckBox) convertView.findViewById(R.id.checkBox);
+
+
+            viewHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        itemChecked[position] = isChecked;
+                }
+            });
 
             convertView.setTag(viewHolder);
 
-        }
-        else {
-
-            viewHolder=(ViewHolder)convertView.getTag();
-        }
-
-        viewHolder.textView.setText(countriesList.get(position).toString());
-
+        viewHolder.checkBox.setChecked(itemChecked[position]);
+        viewHolder.textView.setText(country.getName());
         return convertView;
     }
 
     private class  ViewHolder{
 
         TextView textView;
+        CheckBox checkBox;
+
     }
 
     //Returns a filter that can be used to constrain data with a filtering pattern.
@@ -116,9 +120,9 @@ public class CountryAdapter extends BaseAdapter implements Filterable {
                 for(int i=0;i<filteredCountries.size();i++){
                     
 
-                    if(filteredCountries.get(i).contains(constraint)) {
+                    if(filteredCountries.get(i).getName().contains(constraint)) {
 
-                        filterList.add(filteredCountries.get(i));
+                        filterList.add(filteredCountries.get(i).getName());
 
                     }
                 }
@@ -145,6 +149,7 @@ public class CountryAdapter extends BaseAdapter implements Filterable {
         @Override
         protected void publishResults(CharSequence constraint,
                                       Filter.FilterResults results) {
+
 
             countriesList=(ArrayList<String>)results.values;
 
